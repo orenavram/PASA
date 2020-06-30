1499c78d66b732a0759e51707154df29457f6a73
Oren <orenavram@gmail.com>
2020-06-30 10:15:30 +0300

    Fixations

--- src/backend/pipeline/DatabaseRecord.py
+++ src/backend/pipeline/DatabaseRecord.py
@@ -1,21 +1,26 @@
+import logging
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger()
+legal_iso_types = ('M', 'G', 'A', 'A1', 'A2', 'E', 'D', 'unknown')
+legal_chain_types = ('IGH', 'IGK', 'IGL')
+
 class Record:
 
+
     def __init__(self, db_name, seq, chain_type, iso_type, cdr3, IGHV, IGHD, IGHJ, counts, reps_intensities):
+        if chain_type not in legal_chain_types or iso_type not in legal_iso_types:
+            raise ValueError
+
         self.db_num = db_name
         self.seq = seq
-        # self.ibd = ibd
         self.chain_type = chain_type
         self.iso_type = iso_type
         self.cdr3 = cdr3
-        # self.cdr3_start = -1
-        # self.cdr3_end = -1
         self.IGHV = IGHV
         self.IGHD = IGHD
         self.IGHJ = IGHJ
         self.counts = counts
         self.reps_intensities = [int(x) for x in reps_intensities.split(',')]
-        # self.avg_counts = counts / len(self.reps_intensities) if reps_intensities != None else self.counts
-        # self.enrichment = 0
 
     def __repr__(self):
         return '<' + str([self.db_num, self.seq, self.chain_type, self.iso_type, self.cdr3,
