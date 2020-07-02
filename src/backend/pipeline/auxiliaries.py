ec6f418bccd9a32d6ba43148b373c8c819bb5a2c
Oren <orenavram@gmail.com>
2020-07-02 19:13:30 +0300

    Adding progress update for the UI when MaxQuant is running

--- src/backend/pipeline/auxiliaries.py
+++ src/backend/pipeline/auxiliaries.py
@@ -1,4 +1,5 @@
 import os
+import subprocess
 import logging
 logger = logging.getLogger()
 from time import sleep
@@ -15,7 +16,8 @@ def measure_time(total):
         return f'{seconds} seconds'
 
 
-def wait_for_maxquant(maxquant_wd, time_passed=0, sleeping_time=3):
+def wait_for_maxquant(maxquant_wd, time_passed=0, sleeping_time=5, html_path='',
+                      total_bar_for_maxquant=50, initial_shift=20):
     maxquant_running_times_file = f'{maxquant_wd}/combined/proc/#runningTimes.txt'
     maxquant_result_file = f'{maxquant_wd}/combined/txt/peptides.txt'
     maxquant_done_file = f'{maxquant_wd}/combined/proc/Finish_writing_tables 11.finished.txt'
@@ -23,6 +25,16 @@ def wait_for_maxquant(maxquant_wd, time_passed=0, sleeping_time=3):
     logger.info(f'Progress can be viewed at: {maxquant_running_times_file}')
     while True:
         sleep(sleeping_time)
+        if html_path:
+            # b'  54 maxquant_analysis/el/combined/proc/#runningTimes.txt\n  54 maxquant_analysis/ft/combined/proc/#runningTimes.txt\n 108 total\n'
+            total_steps_done = sum(int(x) for x in subprocess.check_output(f'wc -l {maxquant_wd}/../*/combined/proc/#runningTimes.txt',
+                                                                           shell=True).decode().rstrip().split()[:3][::2])
+            try:
+                # "#runningTimes.txt" file contains 54 rows when it's done. 2 files contains 108...
+                edit_progress(html_path, progress=total_bar_for_maxquant * total_steps_done / 108 + initial_shift)
+            except:
+                logger.info('Could not find any #runningTimes.txt file to estimate progress...')
+                pass
         time_passed += sleeping_time
         logger.info(f'\t{measure_time(time_passed)} have passed since started waiting.')
         if len([file for file in os.listdir(maxquant_wd) if file.endswith('ER')]) > 0:
@@ -36,6 +48,10 @@ def wait_for_maxquant(maxquant_wd, time_passed=0, sleeping_time=3):
 
 def edit_progress(html_path, progress=None, active=True):
     result = ''
+    if not os.path.exists('/bioseq'):
+        # local run
+        return
+
     with open(html_path) as f:
         for line in f:
             if 'progress-bar' in line:
