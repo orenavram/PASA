59bccebb247b600ffed361fc6091d0ea552968e6
Oren <orenavram@gmail.com>
2020-07-02 19:14:35 +0300

    Adding some restrictions for the db_record class

--- src/backend/pipeline/DatabaseRecord.py
+++ src/backend/pipeline/DatabaseRecord.py
@@ -1,14 +1,13 @@
 import logging
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger()
-legal_iso_types = ('M', 'G', 'A', 'A1', 'A2', 'E', 'D', 'unknown')
-legal_chain_types = ('IGH', 'IGK', 'IGL')
+legal_iso_types = ('M', 'G', 'A', 'A1', 'A2', 'E', 'D', 'unknown', 'NONE')
 
 class Record:
 
 
-    def __init__(self, db_name, seq, chain_type, iso_type, cdr3, IGHV, IGHD, IGHJ, counts, reps_intensities):
-        if chain_type not in legal_chain_types or iso_type not in legal_iso_types:
+    def __init__(self, db_name, seq, chain_type, iso_type, cdr3, IGHV, IGHD, IGHJ, counts, reps_intensities=''):
+        if chain_type != 'IGH' or iso_type not in legal_iso_types:
             raise ValueError
 
         self.db_num = db_name
@@ -20,7 +19,11 @@ class Record:
         self.IGHD = IGHD
         self.IGHJ = IGHJ
         self.counts = counts
-        self.reps_intensities = [int(x) for x in reps_intensities.split(',')]
+        if not reps_intensities:
+            # to handle non-joint analysis
+            self.reps_intensities = [1]
+        else:
+            self.reps_intensities = [int(x) for x in reps_intensities.split(',')]
 
     def __repr__(self):
         return '<' + str([self.db_num, self.seq, self.chain_type, self.iso_type, self.cdr3,
